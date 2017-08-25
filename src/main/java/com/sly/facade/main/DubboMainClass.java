d575324a6172c1a9dbd7e19cc66d3e1c13bda4fd
孙立阳 <1204007548@qq.com>
2017-08-25 16:18:13 +0800

    second commit

---
+++ src/main/java/com/sly/facade/main/DubboMainClass.java
@@ -0,0 +1,7 @@
+package com.sly.facade.main;
+
+public class DubboMainClass {
+	public static void main(String[] args) {
+	    com.alibaba.dubbo.container.Main.main(args);
+	}
+}
---
+++ src/test/java/com/sly/test/tt.java
@@ -0,0 +1,7 @@
+package com.sly.test;
+
+public class tt {
+	public static void main(String[] args) {
+	    com.alibaba.dubbo.container.Main.main(args);
+	}
+}
